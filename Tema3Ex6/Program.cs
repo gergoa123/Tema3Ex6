dcc773c285d52d6eedd4475d3632afd0a0e5de11
Gergoa123 <gergo3124@gmail.com>
2022-05-09 18:52:16 +0300

    Add project files.

---
+++ Tema3Ex6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tema3Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul");
+            int numar = int.Parse(Console.ReadLine());
+            int i = 1;
+            int aux = 0;
+
+            while (i != numar)
+            {
+                if (numar % i == 0)
+                {
+                    aux = aux + 1;
+                }
+
+                i++;
+            }
+
+            if (aux == 1)
+            {
+                Console.Write(numar + " Este numar prim!");
+            }
+            else
+            {
+                Console.Write(numar + " Nu este numar prim!");
+            }
+
+
+
+        }
+    }
+}
